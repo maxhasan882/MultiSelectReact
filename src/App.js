4aa7406dad2dc7b0a2d25d92a356776c10a0b697
Hasan <razibulhasan.bs23@gmail.com>
2020-07-07 19:36:12 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Multiselect } from "multiselect-react-dropdown";
+import "./styles.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      plainArray: ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"],
+      objectArray: [
+        { key: "Option 1", cat: "Group 1" },
+        { key: "Option 2", cat: "Group 1" },
+        { key: "Option 3", cat: "Group 1" },
+        { key: "Option 4", cat: "Group 2" },
+        { key: "Option 5", cat: "Group 2" },
+        { key: "Option 6", cat: "Group 2" },
+        { key: "Option 7", cat: "Group 2" },
+      ],
+      selectedValues: [
+        { key: "Option 1", cat: "Group 1" },
+        { key: "Option 2", cat: "Group 1" },
+      ],
+    };
+    this.style = {
+      chips: {
+        background: "red",
+      },
+      searchBox: {
+        border: "none",
+        "border-bottom": "1px solid blue",
+        "border-radius": "0px",
+      },
+      multiselectContainer: {
+        color: "red",
+      },
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem() {
+    this.selectedValues.push({ key: "Option 3", cat: "Group 1" });
+  }
+
+  render() {
+    const { plainArray, objectArray, selectedValues } = this.state;
+    return (
+      <div className="App">
+        <h4 id="pre" className="mt40">
+          3. Multiselect with preselect values
+        </h4>
+        <Multiselect
+          options={objectArray}
+          displayValue="key"
+          selectedValues={selectedValues}
+        />
+      </div>
+    );
+  }
 }
 
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Multiselect } from "multiselect-react-dropdown";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      plainArray: ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"],
+      objectArray: [
+        { key: "Option 1", cat: "Group" },
+        { key: "Option 2", cat: "Group" },
+        { key: "Option 3", cat: "Group" },
+        { key: "Option 4", cat: "Group" },
+      ],
+      selectedValues: [{}],
+    };
+    this.style = {
+      chips: {
+        background: "red",
+      },
+      searchBox: {
+        border: "none",
+        "border-bottom": "1px solid blue",
+        "border-radius": "0px",
+      },
+      multiselectContainer: {
+        color: "red",
+      },
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem(obj) {
+    this.selectedValues.push({});
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render() {
+    const { plainArray, objectArray, selectedValues } = this.state;
+    return (
+      <div className="App">
+        <h4 id="pre" className="mt40">
+          Multiselect Dropdown
+        </h4>
+        <Multiselect options={objectArray} displayValue="key" />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
